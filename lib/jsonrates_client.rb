d96dd3af06ee7f5b4a7bdfea6c916dd14d3b39e1
Roberto Soares <roberto.tech@gmail.com>
2014-09-25 22:19:32 -0300

    historical: replacing date_start and date_end with dateStart and dateEnd

--- lib/jsonrates_client.rb
+++ lib/jsonrates_client.rb
@@ -6,9 +6,25 @@ module Jsonrates
     include HTTParty
     base_uri "jsonrates.com"
 
+    REPLACE_KEYS = {
+      date_start: :dateStart,
+      date_end: :dateEnd
+    }
+
     def self.historical(options)
+      prepare_params(options)
       response = JSON.parse(get("/historical", query: options))
       response["rates"]
+    end
+
+    private
+
+    def self.prepare_params(options)
+      REPLACE_KEYS.each_key do |key|
+        if options.has_key?(key)
+          options[REPLACE_KEYS[key]] = options.delete(key)
+        end
+      end
     end
   end
 end
--- spec/jsonrates_client_spec.rb
+++ spec/jsonrates_client_spec.rb
@@ -17,5 +17,15 @@ RSpec.describe Jsonrates::Client do
         expect(historical).to eq({"2014-06-23"=>{"utctime"=>"2014-06-23T23:50:03+02:00", "rate"=>"2.22120000"}})
       end
     end
+
+    describe "params keys setup" do
+      let!(:historical) do
+        subject.historical(base: "USD", date_start: "2014-06-23", date_end: "2014-06-25")
+      end
+
+      it "replaces date_start and date_end with dateStart and dateEnd" do
+        expect(a_request(:get, "http://jsonrates.com/historical").with(query: {base: "USD",  dateStart: "2014-06-23", dateEnd: "2014-06-25"})).to have_been_made
+      end
+    end
   end
 end
